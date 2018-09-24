4f969c41b117f726ee060ec1228b9ae45aa16af1
Saravanan <Saravanan.Narayanan@test.com>
2018-09-24 18:32:31 +0530

    Updated builder.py

--- Builder.py
+++ Builder.py
@@ -11,8 +11,8 @@ print("**********************************************************")
 
 cwd = os.getcwd()
 repoPath = sys.argv[1]
-#os.chdir(repoPath)
-#
+os.chdir(repoPath)
+
 inputfile1 = open('BuildComponent_1.txt')
 inputfile2 = open('BuildComponent_2.txt')
 inputfile3 = open('BuildComponent_3.txt')
